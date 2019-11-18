37434e449d7ca6b5d747ad08283beaac12b8905c
Ryan Nugraha <ryan@skoop.com.au>
2019-11-19 09:06:56 +1100

    Continuation

--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -1,34 +1,18 @@
 import React from 'react'
+import { Link, Router, Routes, useDataServer } from 'docz'
 import { hot } from 'react-hot-loader'
 import Theme from 'docz-theme-default'
 
-const socket = new WebSocket(`ws://127.0.0.1:8089`)
+import { imports } from './imports'
+import database from './db.json'
 
-class Root extends React.Component {
-  state = {
-    config: {},
-    entries: {},
-  }
-
-  async componentDidMount() {
-    socket.onmessage = ev => {
-      const message = JSON.parse(ev.data)
-
-      if (message.type === 'docz.entries') {
-        this.setState({ entries: message.data })
-      }
-
-      if (message.type === 'docz.config') {
-        this.setState({ config: message.data })
-      }
-    }
-  }
-
-  render() {
-    const { imports } = this.props
-
-    return <Theme {...this.state} imports={imports} hashRouter={false} />
-  }
+const Root = () => {
+  useDataServer('ws://127.0.0.1:60505')
+  return (
+    <Theme linkComponent={Link} db={database}>
+      <Routes imports={imports} />
+    </Theme>
+  )
 }
 
 export default hot(module)(Root)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,72 @@
-import React, { useEffect, Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import ReactDOM from 'react-dom';
+import propTypes, { node, string, oneOfType } from 'prop-types';
 
-const Tooltip = () => {
-  const domNode = document.createElement('div');
-  document.body.appendChild(domNode);
-  return <h3>HI</h3>;
+const Tooltip = props => {
+  const [isDisplay, setIsDisplay] = useState(props.defaultDisplay || false);
+
+  const id = props.id || 'nuggie-tooltip-default';
+  const children = props.children || null;
+  const render = props.render || null;
+
+  let domNode = document.getElementById(id);
+  if (!domNode) {
+    domNode = document.createElement('div');
+    domNode.setAttribute('id', id);
+    domNode.setAttribute('key', id);
+    document.body.appendChild(domNode);
+  }
+
+  useEffect(() => {
+    renderToolTip();
+  }, [isDisplay]);
+
+  const handleMouseEnter = () => {
+    setIsDisplay(!isDisplay);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDisplay(!isDisplay);
+  };
+
+  const handleFocus = () => {
+    setIsDisplay(!isDisplay);
+  };
+
+  const handleBlur = () => {
+    setIsDisplay(!isDisplay);
+  };
+
+  const renderToolTip = () => {
+    if (!isDisplay) {
+      return null;
+    }
+
+    return ReactDOM.createPortal(
+      <h2 style={{ position: 'absolute', top: '50%', left: '50%' }}>{render}</h2>,
+      domNode,
+    );
+  };
+
+  const tooltipWrapper = () => {
+    return (
+      <span
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      >
+        {children}
+      </span>
+    );
+  };
+
+  return [tooltipWrapper(), renderToolTip()];
 };
+
+// Tooltip.propTypes = {
+//   children: node.isRequired,
+//   render: onOfType([node, string]).isRequired,
+// };
 
 export default Tooltip;
